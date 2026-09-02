1ce5d2aab1723ef3b468ef8d8bd09141ad669251
Tristan Penman <tristan@tristanpenman.com>
2026-09-03 09:33:33 +1000

    Allow passing a command directly to python shells

--- scripts/python-shell-gpu.sh
+++ scripts/python-shell-gpu.sh
@@ -7,5 +7,10 @@ docker compose build python-gpu
 echo "==> Starting GPU container"
 HOST_UID="$(id -u)" HOST_GID="$(id -g)" docker compose up --remove-orphans -d python-gpu
 
-echo "==> Starting shell"
-docker compose exec python-gpu /usr/local/bin/docker-entrypoint.sh bash
+if [ "$#" -eq 0 ]; then
+  echo "==> Starting shell"
+else
+  echo "==> Running command"
+fi
+
+docker compose exec python-gpu /usr/local/bin/docker-entrypoint.sh "$@"
